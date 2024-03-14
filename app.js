7fdaff043a082f49d8c54db02cdefbab630b8d36
GonzaloArmoa <gonzalo-armoa@hotmail.com>
2024-03-14 15:03:08 -0300

    feat: add db conection

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// ************ Require's ************
+require('dotenv').config()
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+// ************ express() - (don't touch) ************
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// ************ Middlewares - (don't touch) ************
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,12 +26,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+
+
+
+// ************ DON'T TOUCH FROM HERE ************
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// ************ error handler ************
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -40,4 +49,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// ************ exports app - dont'touch ************
 module.exports = app;
